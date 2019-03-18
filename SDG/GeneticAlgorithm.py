fe3f83d699c70608ced8ebf9a2ba8ff599600c31
MryoungYu <youngyu2012@sina.com>
2019-03-18 23:13:55 +0800

    GA

--- SDG/GeneticAlgorithm.py
+++ SDG/GeneticAlgorithm.py
@@ -13,6 +13,7 @@ class GeneticAlgorithm:
 
     def __init__(self, gdgs):
         gene_list = []
+        self.chromosome_num = len(gdgs)
         for gdg in gdgs:
             self.group_name = gdg.group_name
             ch = Chromosome(gdg)
@@ -21,15 +22,37 @@ class GeneticAlgorithm:
         self.genes_repo = list(set(gene_list))
 
     def exec(self):
+        # 直接进入下一生代的染色体个数
         M = 2
-        th_new, th_cross= self.select(M)
+        # 选择算子-1
+        th_new = self.select_next(M)
         th_next_generation = th_new
-        for each_th_cross in th_cross:
-            each_th_variation = self.cross(each_th_cross)
-            each_th_ga = self.variation(each_th_variation)
-            if self.check(each_th_ga):
-                th_next_generation.append(each_th_ga)
+        while len(th_next_generation) < self.chromosome_num:
+            # 选择算子-2
+            th_cross_1, th_cross_2 = self.select_croos()
+            # 交叉算子
+            th_variation = self.cross(th_cross_1, th_cross_2)
+            # 变异算子
+            th_ga = self.variation(th_variation)
+            # 后置检查
+            if self.check(th_ga):
+                th_next_generation.append(th_ga)
+        # 生代加1
+        self.current_generation += 1
+        return th_next_generation
 
+    def select_next(self, M):
+        """选择直接进入下一生代的Top M个染色体"""
 
+    def select_cross(self):
+        """以概率的方式选择两个染色体进入交叉算子"""
 
+    def cross(self, th1, th2):
+        """交叉算子"""
 
+    def variation(self, th):
+        """变异算子"""
+
+    def check(self, th):
+        """后置检查"""
+        return True
