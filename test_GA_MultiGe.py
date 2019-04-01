720e4313c0584aa7d8c5d274ecf1f04316e64a5d
MryoungYu <youngyu2012@sina.com>
2019-04-01 20:04:12 +0800

    test ga

--- test_GA_MultiGe.py
+++ test_GA_MultiGe.py
@@ -1,6 +1,7 @@
 from SDG.Chromosome import *
 import random
 import time
+import operator
 
 # print("生成安全事件队列")
 # for i in range(10):
@@ -45,14 +46,14 @@ genes_repo = range(21)
 ch1 = Chromosome()
 genes_list_1 = [20, 1, 2, 10, 17, 12, 4, 5, 9]
 ch1.gene_list = genes_list_1
-ch1.fitness = 0.95
+ch1.fitness = 0.9211
 ch1.total_threat = 3
 ch1.ch_name = 'ch1'
 
 ch2 = Chromosome()
 genes_list_2 = [18, 2, 8, 19, 5, 14]
 ch2.gene_list = genes_list_2
-ch2.fitness = 0.8667
+ch2.fitness = 0.8947
 ch2.total_threat = 4
 ch2.ch_name = 'ch2'
 
@@ -66,49 +67,49 @@ ch3.ch_name = 'ch3'
 ch4 = Chromosome()
 genes_list_4 = [8, 10, 13, 15, 0, 1, 18, 5, 3]
 ch4.gene_list = genes_list_4
-ch4.fitness = 0.8333
+ch4.fitness = 0.8158
 ch4.total_threat = 7
 ch4.ch_name = 'ch4'
 
 ch5 = Chromosome()
 genes_list_5 = [7, 11, 16, 19, 4, 17, 1]
 ch5.gene_list = genes_list_5
-ch5.fitness = 0.8
+ch5.fitness = 0.8421
 ch5.total_threat = 6
 ch5.ch_name = 'ch5'
 
 ch6 = Chromosome()
 genes_list_6 = [15, 0, 17, 4, 15, 2, 10]
 ch6.gene_list = genes_list_6
-ch6.fitness = 0.95
+ch6.fitness = 0.9737
 ch6.total_threat = 1
 ch6.ch_name = 'ch6'
 
 ch7 = Chromosome()
 genes_list_7 = [19, 5, 17, 15, 4, 14, 7, 16]
 ch7.gene_list = genes_list_7
-ch7.fitness = 0.9167
+ch7.fitness = 0.9211
 ch7.total_threat = 3
 ch7.ch_name = 'ch7'
 
 ch8 = Chromosome()
 genes_list_8 = [0, 10, 9, 5, 20, 8]
 ch8.gene_list = genes_list_8
-ch8.fitness = 0.9333
+ch8.fitness = 0.9211
 ch8.total_threat = 3
 ch8.ch_name = 'ch8'
 
 ch9 = Chromosome()
 genes_list_9 = [4, 9, 1, 7, 5, 9, 11, 18]
 ch9.gene_list = genes_list_9
-ch9.fitness = 0.8667
+ch9.fitness = 0.8684
 ch9.total_threat = 5
 ch9.ch_name = 'ch9'
 
 ch10 = Chromosome()
 genes_list_10 = [4, 1, 14, 9, 20]
 ch10.gene_list = genes_list_10
-ch10.fitness = 0.8833
+ch10.fitness = 0.8421
 ch10.total_threat = 6
 ch10.ch_name = 'ch10'
 
@@ -126,7 +127,7 @@ for ch in ch_total_list:
         gene_threat_dict[gene] += t_ge
 print("基因库威胁值：")
 print(gene_threat_dict)
-exit(-1)
+
 # 遗传算法
 generation_max = 100 # 最大生代
 generation_current = 0 # 当前生代
@@ -138,14 +139,25 @@ ch_repo = set() # 染色体仓库
 
 u = 5 # 每一生代直接留存的染色体个数
 n = 5 # 生成新染色体个数
-m = 0 # 畸变个数
-k = 0 # 当前已生成染色体个数
-while k < n:
-# for inter in range(n):
-    # 选择染色体
-    r = random.uniform(0, ch_fitness_total)
-    # print("Random = " + str(r))
-    choose_ch = 0.0
+
+while generation_current < generation_max:
+    current_chromosomes = ch_total_list
+    next_chromosoomes = list()
+    m = 0 # 畸变个数
+    k = 0 # 当前已生成染色体个数
+    # 选择算子：选取适应度最高的染色体
+    cmpfun = operator.attrgetter('fitness')
+    current_chromosomes.sort(key=cmpfun, reverse=True)
+    next_chromosomes = current_chromosomes[0:u]
+
+    exit(-1)
+
+    while k < n:
+    # for inter in range(n):
+        # 选择染色体
+        r = random.uniform(0, ch_fitness_total)
+        # print("Random = " + str(r))
+        choose_ch = 0.0
     chosen_ch_1 = None
     for ch in ch_total_list:
         if choose_ch >= r:
